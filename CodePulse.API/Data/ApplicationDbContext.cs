b371dd3117622d950a5f0e37afa346314a9979d9
benito JD <benito.josede@gmail.com>
2024-12-21 21:22:49 +0530

    Add project files.

---
+++ CodePulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
